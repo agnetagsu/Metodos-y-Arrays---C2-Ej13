228a25dfd2f213d661ca3db201dd60a2857ea6a2
Agneta <agnetagsu@yahoo.es>
2021-05-23 12:27:40 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] miarray;
+            int longitud;
+            Random aleatorio = new Random();
+
+            Console.WriteLine("Introduce la longitud del array:");
+            longitud = Convert.ToInt32(Console.ReadLine());
+            miarray = new int[longitud];
+
+
+            for (int i = 0; i < miarray.Length; i++)
+            {
+                miarray[i] = aleatorio.Next(1,10);
+                Console.WriteLine(miarray[i]);
+            }
+            
+        }
+    }
+}
